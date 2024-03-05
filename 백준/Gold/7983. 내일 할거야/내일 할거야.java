e79441ade7b8443529b7091a7e278f25723c06cd
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-03-06 08:42:18 +0900

    [Gold V] Title: 내일 할거야, Time: 1676 ms, Memory: 281268 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static class HomeWork implements Comparable<HomeWork>{
+		int d;
+		int t;
+		public HomeWork(int d, int t) {
+			super();
+			this.d = d;
+			this.t = t;
+		}
+		@Override
+		public int compareTo(HomeWork o) {
+			// TODO Auto-generated method stub
+			return o.t - this.t;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		
+		PriorityQueue<HomeWork> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			pq.add(new HomeWork(d, t));
+		}
+		int startD = pq.peek().t;
+		
+		while(!pq.isEmpty()) {
+			HomeWork h = pq.poll();
+			
+			if (h.t <= startD) {
+				startD = h.t - h.d;
+			} else {
+				startD -= h.d;
+			}
+		}
+		
+		System.out.println(startD);
+	}
+}
