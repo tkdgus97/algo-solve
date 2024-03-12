d80bc6c696966c5221cbbd427c5fe389176a63db
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-03-12 23:28:26 +0900

    [Gold V] Title: 내일 할거야, Time: 1620 ms, Memory: 281000 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -1,52 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
+//문제 풀이용
 public class Main {
-	private static class HomeWork implements Comparable<HomeWork>{
-		int d;
-		int t;
-		public HomeWork(int d, int t) {
-			super();
-			this.d = d;
-			this.t = t;
-		}
-		@Override
-		public int compareTo(HomeWork o) {
-			// TODO Auto-generated method stub
-			return o.t - this.t;
-		}
-		
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    private static class HomeWork implements Comparable<HomeWork>{
+        int d;
+        int t;
+        public HomeWork(int d, int t) {
+            super();
+            this.d = d;
+            this.t = t;
+        }
+        @Override
+        public int compareTo(HomeWork o) {
+            // TODO Auto-generated method stub
+            return o.t - this.t;
+        }
 
-		int N = Integer.parseInt(st.nextToken());
-		
-		PriorityQueue<HomeWork> pq = new PriorityQueue<>();
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			pq.add(new HomeWork(d, t));
-		}
-		int startD = pq.peek().t;
-		
-		while(!pq.isEmpty()) {
-			HomeWork h = pq.poll();
-			
-			if (h.t <= startD) {
-				startD = h.t - h.d;
-			} else {
-				startD -= h.d;
-			}
-		}
-		
-		System.out.println(startD);
-	}
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<HomeWork> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            pq.add(new HomeWork(d, t));
+        }
+
+        int startD = pq.peek().t;
+
+        while (!pq.isEmpty()) {
+            HomeWork h = pq.poll();
+
+            if (h.t <= startD) {
+                startD = h.t - h.d;
+            } else {
+                startD -= h.d;
+            }
+        }
+
+        System.out.println(startD);
+    }
 }
