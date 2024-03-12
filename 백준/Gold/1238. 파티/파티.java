90e54948043176a6165753caf3c5684985669ddd
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-03-12 12:51:17 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 17240 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,103 +3,106 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static class Node {
+	private static class Node{
 		List<Edge> edges;
 
-		public Node() {
-			this.edges = new ArrayList<>();
+		public Node(List<Edge> edges) {
+			this.edges = edges;
 		}
-
+		
+		
 	}
-
+	
 	private static class Edge implements Comparable<Edge>{
-		int to;
-		int t;
-
-		public Edge(int to, int t) {
-			this.to = to;
-			this.t = t;
+		int from;
+		int w;
+		public Edge(int from, int w) {
+			super();
+			this.from = from;
+			this.w = w;
 		}
-
 		@Override
 		public int compareTo(Edge o) {
-			return this.t - o.t;
+			return this.w - o.w;
 		}
-
-
+		
+		
 	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		
 		int X = Integer.parseInt(st.nextToken());
-
+		
+		//돌아갈때
 		Node[] nodes = new Node[N + 1];
-		Node[] reverse = new Node[N + 1];
-
+		//각 지점에서 올때
+		Node[] reverseNode = new Node[N + 1];
+		
 		for (int i = 1; i <= N; i++) {
-			nodes[i] = new Node();
-			reverse[i] = new Node();
+			nodes[i] = new Node(new ArrayList<>());
+			reverseNode[i] = new Node(new ArrayList<>());
 		}
-
+		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-
-			nodes[from].edges.add(new Edge(to, t));
-			reverse[to].edges.add(new Edge(from, t));
+			int from = Integer.parseInt(st.nextToken());;
+			int w = Integer.parseInt(st.nextToken());
+			nodes[to].edges.add(new Edge(from, w));	
+			reverseNode[from].edges.add(new Edge(to, w));
 		}
-
-		int[] distance = new int[N + 1];
-		int[] reverseDistance = new int[N + 1];
-		Arrays.fill(distance, Integer.MAX_VALUE);
-		Arrays.fill(reverseDistance, Integer.MAX_VALUE);
-		distance[X] = reverseDistance[X] = 0;
 		
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
 		
+		int[] goDis = new int[N + 1];
+		int[] backDis = new int[N + 1];
+		
+		Arrays.fill(goDis, Integer.MAX_VALUE);
+		Arrays.fill(backDis, Integer.MAX_VALUE);
+		
+		goDis[X] = backDis[X] = 0;
+		
 		pq.add(new Edge(X, 0));
-		while (!pq.isEmpty()) {
+		
+		while(!pq.isEmpty()) {
 			Edge now = pq.poll();
 			
-			for (Edge e : nodes[now.to].edges) {
-				if (distance[e.to] > e.t + now.t) {
-					distance[e.to] = now.t + e.t;
-					pq.add(new Edge(e.to, now.t + e.t));
+			for (int i = 0; i < nodes[now.from].edges.size(); i++) {
+				if (backDis[nodes[now.from].edges.get(i).from] > now.w + nodes[now.from].edges.get(i).w) {
+					backDis[nodes[now.from].edges.get(i).from] = now.w + nodes[now.from].edges.get(i).w;
+					pq.add(new Edge(nodes[now.from].edges.get(i).from, now.w + nodes[now.from].edges.get(i).w));
 				}
-			}
-
+			} 
 		}
 		
 		pq.add(new Edge(X, 0));
-		while (!pq.isEmpty()) {
+		
+		while(!pq.isEmpty()) {
 			Edge now = pq.poll();
 			
-			for (Edge e : reverse[now.to].edges) {
-				if (reverseDistance[e.to] > e.t + now.t) {
-					reverseDistance[e.to] = now.t + e.t;
-					pq.add(new Edge(e.to, now.t + e.t));
+			for (int i = 0; i < reverseNode[now.from].edges.size(); i++) {
+				if (goDis[reverseNode[now.from].edges.get(i).from] > now.w + reverseNode[now.from].edges.get(i).w) {
+					goDis[reverseNode[now.from].edges.get(i).from] = now.w + reverseNode[now.from].edges.get(i).w;
+					pq.add(new Edge(reverseNode[now.from].edges.get(i).from, now.w + reverseNode[now.from].edges.get(i).w));
 				}
-			}
-
+			} 
 		}
 		
 		int result = Integer.MIN_VALUE;
-		
 		for (int i = 1; i <= N; i++) {
-			result = Math.max(result,  distance[i] + reverseDistance[i]);
+			result = Math.max(goDis[i] + backDis[i], result);
 		}
+		
 		System.out.println(result);
 	}
 }
