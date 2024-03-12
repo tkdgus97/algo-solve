8c1ed2d98e4ba4d5e2e03f32c0d4da4ee117cebe
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-03-12 10:31:54 +0900

    [Gold IV] Title: 우체국, Time: 488 ms, Memory: 46668 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,46 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-//문제 풀이용
 public class Main {
-    private static class Village{
-        long v;
-        long p;
-        public Village(long v, long p) {
-            this.v = v;
-            this.p = p;
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	private static class Home implements Comparable<Home>{
+		long p;
+		long k;
+		public Home(long p, long k) {
+			super();
+			this.p = p;
+			this.k = k;
+		}
+		@Override
+		public int compareTo(Home o) {
+			// TODO Auto-generated method stub
+			return Long.compare(this.p, o.p);
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		Home[] homes = new Home[N];
+		
+		long total = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 
-        List<Village> list = new ArrayList<>();
-        long total = 0;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-            list.add(new Village(v,p));
-            total += p;
-        }
+			homes[i] = new Home(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+			
+			total += homes[i].k;
+		}
+		
 
-        Collections.sort(list, (o1, o2) -> Long.compare(o1.v , o2.v));
-
-        long people = 0;
-
-        for (Village village : list) {
-            people += village.p;
-            if ((total + 1) / 2 <= people) {
-                System.out.println(village.v);
-                break;
-            }
-        }
-    }
-
+		Arrays.sort(homes);
+		
+		total = (total + 1)/2;
+		long tmp = 0;
+		for (int i = 0; i < N; i++) {
+			tmp += homes[i].k;
+			if (tmp >= total) {
+				System.out.println(homes[i].p);
+				break;
+			}
+		}
+		
+		
+	}
 }
