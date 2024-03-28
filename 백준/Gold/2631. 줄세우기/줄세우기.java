c3645d0095b4c0954c41b9c755fc87a98dc1a7e8
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-03-28 23:21:42 +0900

    [Gold IV] Title: 줄세우기, Time: 80 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] values = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (values[i] > values[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(N - max);
+    }
+
+
+}
