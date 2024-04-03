cd4d635569a936b8d32651fd9f9f5f9e4b7a2d70
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-04-03 22:31:37 +0900

    [Gold III] Title: 앱, Time: 108 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,61 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+        int[] memory = new int[N + 1];
+        int[] costs = new int[N + 1];
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[] mem = new int[N];
-		int[] costs = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			mem[i] = Integer.parseInt(st.nextToken());
-		}
-		st = new StringTokenizer(br.readLine());
-		int cTotal = 0;
-		for (int i = 0; i < N; i++) {
-			costs[i] = Integer.parseInt(st.nextToken());
-			cTotal += costs[i];
-		}
-		
-		int[][] dp = new int[N][cTotal + 1];
-        int result = Integer.MAX_VALUE;
-		for (int i = 0; i <= cTotal ; i++) {
-			if (i >= costs[0]) {
-				dp[0][i] = mem[0];
-			}
-			if (dp[0][i] >= M) {
-				result = Math.min(result, i);
-			}
-		}
-	
-		for (int i = 1; i < N; i++) {
-			int cost = costs[i];
-			int m = mem[i];
-			
-			for (int j = 0; j <= cTotal; j++) {
-				if(j >= cost) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + m);					
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
-				if (dp[i][j] >= M) {
-					result = Math.min(result, j);
-				}
-			}
-		}
-		
-		System.out.println(result);
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            memory[i] = Integer.parseInt(st.nextToken());
+        }
+        int costTotal = 0;
+        int min = Integer.MAX_VALUE;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            costs[i] = Integer.parseInt(st.nextToken());
+            costTotal += costs[i];
+            if (memory[i] >= M) {
+                min = Math.min(min, costs[i]);
+            }
+        }
 
+        int[][] dp = new int[N + 1][costTotal + 1];
+
+        for (int i = 1; i <= N; i++) {
+            int mem = memory[i];
+            int cost = costs[i];
+            for (int j = 0; j <= costTotal; j++) {
+                if (j >= cost) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + mem);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+
+                if (dp[i][j] >= M) {
+                    min = Math.min(min, j);
+                }
+            }
+        }
+        System.out.println(min);
+    }
 }
