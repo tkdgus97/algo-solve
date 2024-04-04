de465ba1b505b336c9c6087aa2a17824acdcc8f1
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-04-04 21:50:54 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 432 ms, Memory: 40820 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+//문제 풀이용
+public class Main {
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+
+    private static int[][] map, dp;
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        dp = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                max = Math.max(dfs(i,j), max);
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static int dfs(int x, int y) {
+        if (dp[x][y] != 0) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if ((nx >= 0 && nx < N) && (ny >= 0 && ny < N) && map[x][y] < map[nx][ny]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+                dfs(nx, ny);
+            }
+        }
+
+        return dp[x][y];
+    }
+}
