b6f1b98bc4247fb5af0403801e0f0b234e5f2d55
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-04-25 09:42:23 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.10 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 class Solution {
     public int solution(int coin, int[] cards) {
         int answer = game(cards.length, coin, cards);;
-
         game(cards.length, coin, cards);
 
         return answer;
@@ -14,35 +14,46 @@ class Solution {
 
         int cardIdx = n / 3;
         int target = n + 1;
+
         boolean[] isGetCard = new boolean[n + 1];
         boolean[] trashCard = new boolean[n + 1];
-        List<Integer> nowCards = new ArrayList<>();
 
+        int targetCnt = 0;
         for (int i = 0; i < cardIdx; i++) {
-            nowCards.add(cards[i]);
             isGetCard[cards[i]] = true;
         }
+        for (int i = 0; i < cardIdx; i++) {
+            if (isGetCard[target - cards[i]] && isGetCard[cards[i]]) {
+                isGetCard[target - cards[i]] = false;
+                isGetCard[cards[i]] = false;
+                targetCnt++;
+            }
+        }
+
+        HashSet<Integer> trashC = new HashSet<>();
 
         while (cardIdx < n) {
+            int card1 = cards[cardIdx++], card2 = cards[cardIdx++];
 
-            trashCard[cards[cardIdx++]] = trashCard[cards[cardIdx++]] = true;
+            trashC.add(card1);
+            trashC.add(card2);
+            trashCard[card1] = trashCard[card2] = true;
 
             boolean isGo = false;
-            for (Integer nowCard : nowCards) {
-                if (isGetCard[nowCard] && isGetCard[target - nowCard] && !isGo) {
-                    isGetCard[nowCard] = isGetCard[target - nowCard] = false;
-                    isGo = true;
-                }
+            if (targetCnt > 0) {
+                targetCnt -= 1;
+                isGo = true;
             }
 
             if (!isGo) {
                 if (coin >= 1) {
-                    for (Integer nowCard : nowCards) {
-                        if (isGetCard[nowCard] && trashCard[target - nowCard] && !isGo) {
-                            coin -= 1;
-                            trashCard[target - nowCard] = false;
-                            isGetCard[nowCard] = false;
+                    for (Integer i : trashC) {
+                        if (isGetCard[target - i]) {
+                            isGetCard[i] = false;
                             isGo = true;
+                            coin -= 1;
+                            trashC.remove(i);
+                            break;
                         }
                     }
                 }
@@ -50,11 +61,14 @@ class Solution {
 
             if (!isGo) {
                 if (coin >= 2) {
-                    for (int i = 1; i <= n; i++) {
-                        if (trashCard[i] && trashCard[target - i] && !isGo) {
+                    for (Integer i : trashC) {
+                        if (trashCard[target - i]) {
+                            trashCard[target - i] = trashCard[i] = false;
                             isGo = true;
-                            trashCard[i] = trashCard[target - i] = false;
                             coin -= 2;
+                            trashC.remove(i);
+                            trashC.remove(target - i);
+                            break;
                         }
                     }
                 }
