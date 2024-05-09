e40a48a6635459f6da748ae5f8c79be85b5b473b
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-05-09 21:36:08 +0900

    [Gold III] Title: 스터디 시간 정하기 1, Time: 500 ms, Memory: 92572 KB -BaekjoonHub

---
+++ 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    private static int n;
+    private static StringBuilder stringBuilder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        List<int[]>[] times = new ArrayList[n];
+        long[] sum = new long[100001];
+        int maxTime = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            times[i] = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < k; j++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                maxTime = Math.max(maxTime, e);
+                sum[s]++;
+                sum[e]--;
+            }
+        }
+        for (int i = 1; i <= maxTime; i++) {
+            sum[i] += sum[i - 1];
+        }
+        for (int i = 1; i <= maxTime; i++) {
+            sum[i] += sum[i - 1];
+        }
+        int sTime = 0, eTime = t;
+        long max = sum[t - 1];
+
+        for (int i = t; i < maxTime; i++) {
+            long i1 = sum[i] - sum[i - t];
+            if (max < i1) {
+                sTime = i - t + 1 ;
+                eTime = i + 1;
+                max = i1;
+            }
+        }
+        System.out.println(sTime + " " + eTime);
+    }
+}
