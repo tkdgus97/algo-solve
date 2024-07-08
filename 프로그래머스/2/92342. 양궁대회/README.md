1cdb26ccc8610ad75ac7585581c2cda490732f12
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-07-08 09:30:48 +0900

    [level 2] Title: 양궁대회, Time: 0.93 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 4.02 ms
+메모리: 73.9 MB, 시간: 0.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 05일 16:04:19
+2024년 07월 08일 09:30:45
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,47 +1,66 @@
 class Solution {
-    static private int[] res = new int[11];//점수차가 최대일때 라이언이 쏜 화살배열
-	static private int[] lion= {-1};//정답배열
-	static private int max = Integer.MIN_VALUE;//최대값
-    public static int[] solution(int n, int[] info) {
-        back(0,n,info);//라이언이 쏜 화살에 대한 조합
-        
-        if(max==-1) {//라이언이 어피치를 못 이길떄
-        	lion = new int[1];
-        	lion[0]=-1;
+    private static int[] ryon = new int[11];
+    private static int[] result = new int[11];
+    private static int max = Integer.MIN_VALUE;
+
+    public int[] solution(int n, int[] info) {
+        rec(0, n, info,0);
+
+        if (max == -1) {
+            result = new int[1];
+            result[0] = -1;
         }
-        return lion;
+
+        return result;
     }
-    
-    public static void back(int depth, int n, int[] info) {
-    	//화살 다 꽂았을때(종료조건)
-    	if(depth==n) {
-    		int diff = score(info);//점수차 구하기
-    		if(max<=diff) {//점수차 최대값 갱신
-    			max = diff;
-    			lion = res.clone();
-    		}
-    		return;
-    	}
-    	
-    	//res[i]<=info[i] -> i과녁에 라이언이 화살을 더 많이 맞추면 반복문 종료한다.
-    	for(int i=0; i<info.length && res[i]<=info[i]; i++) {
-    		res[i] += 1;
-    		back(depth+1, n, info);
-    		res[i] -= 1;
-    	}
+
+    private static void rec(int L, int n, int[] info, int start) {
+        if (L == n) {
+            int diff = score(info);
+
+            if (max < diff) {
+                max = diff;
+                result = ryon.clone();
+            } else if(max== diff){
+int o=0;
+                int p= 0;
+for(int i = 0; i<=10; i++){
+    o += ((10-i)* result[i]);
+    p += ((10-i)* ryon[i]);
+}
+                if(p < o){
+result = ryon.clone();
+}
+                
+}
+
+            return;
+        }
+        int i;
+        for (i = start; i <= 10; i++) {
+            if (ryon[i] > info[i]) continue;
+            ryon[i]++;
+            rec(L + 1, n, info,i);
+            ryon[i]--;
+        }
     }
-    
-    //점수차 구하기
-    public static int score(int[] info) {
-    	int apeach=0, lion=0;
-    	for(int i=0; i<res.length; i++) {
-    		if(info[i]==0 && res[i]==0) continue;//i원소에 둘다 0개 맞췄을땐 무시.
-    		if(info[i]>=res[i]) apeach += (10-i);
-    		else lion += (10-i);
-    	}
-    	
-    	int diff = lion - apeach;
-    	if(diff<=0) return -1;
-    	return diff;
+
+    private static int score(int[] info) {
+        int pSum = 0;
+        int rSum = 0;
+        int i;
+        for (i = 0; i < 11; i++) {
+            if (ryon[i] == 0 && info[i] == 0) continue;
+            if (ryon[i] > info[i]) {
+                rSum += (10 - i);
+            } else {
+                pSum += (10 - i);
+            }
+        }
+
+        if (pSum >= rSum) {
+            return -1;
+        }
+        return (rSum - pSum);
     }
 }
