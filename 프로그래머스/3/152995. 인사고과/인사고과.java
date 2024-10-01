a36396c7a11c0a19965f498c59566e0548e4da47
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-10-01 13:40:27 +0900

    [level 3] Title: 인사고과, Time: 170.61 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] scores) {
+        int answer = 1;
+        
+        int n = scores[0][0];
+        int m = scores[0][1];
+    
+        Arrays.sort(scores, (o1,o2) -> {
+            if(o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o2[0] - o1[0];
+        });
+        
+        int maxScore = scores[0][1];
+        
+        for(int i = 0; i < scores.length; i++) {
+            if(scores[i][1] < maxScore) {
+                if(scores[i][0] == n && scores[i][1] == m) return -1;
+                
+                scores[i][0] = -1;
+                scores[i][1] = -1; 
+            } else {
+                maxScore = scores[i][1];
+            }
+        }
+        
+        Arrays.sort(scores, (o1,o2) -> {
+            return (o2[0] + o2[1]) - (o1[0] + o1[1]);
+        });
+        
+        for(int i = 0; i < scores.length; i++) {
+            if(scores[i][0] + scores[i][1] > n + m) answer++;
+            else break;
+        }
+        return answer;
+    }
+}
