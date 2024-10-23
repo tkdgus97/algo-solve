c8f703b13ac8da1297fdb7b1336c73ac102e714b
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-10-23 21:43:57 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1804 ms, Memory: 411132 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -1,56 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static List<Integer>[] trees;
-    private static boolean[] visit;
+    private static List<Integer>[] adj;
     private static int[][] dp;
+    private static boolean[] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
 
-        trees = new ArrayList[n + 1];
+        adj = new ArrayList[n + 1];
+        dp = new int[2][n + 1];
         visit = new boolean[n + 1];
-        dp = new int[n + 1][2];
         for (int i = 1; i <= n; i++) {
-            trees[i] = new ArrayList<>();
-            dp[i][1] = 1;
+            adj[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 1; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            trees[a].add(b);
-            trees[b].add(a);
+            adj[a].add(b);
+            adj[b].add(a);
         }
 
-        func(1);
+        dfs(1);
 
-        System.out.println(Math.min(dp[1][0], dp[1][1]));
+        System.out.println(Math.min(dp[0][1], dp[1][1]));
     }
 
-    private static void func(int node) {
-        visit[node] = true;
+    private static void dfs(int cur) {
+        dp[0][cur] = 0;
+        dp[1][cur] = 1;
+        visit[cur] = true;
+        for (Integer next : adj[cur]) {
+            if (visit[next]) continue;
 
-        for (Integer child : trees[node]) {
-            if (!visit[child]) {
-                func(child);
-                //자신이 얼리어댑터가 아닐경우 자식 모두가 얼리어야 됨
-                dp[node][0] += dp[child][1];
-                //자신이 얼리어댑터라면 자식이 얼리어댑터 or 아닐 수도 있음
-                dp[node][1] += Math.min(dp[child][0], dp[child][1]);
-            }
+            dfs(next);
+            dp[0][cur] += dp[1][next];
+            dp[1][cur] += Math.min(dp[0][next], dp[1][next]);
         }
+
     }
 
 }
