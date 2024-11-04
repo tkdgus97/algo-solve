9497f2cb861e1050fe32a63fd4f89b533571dbaf
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-11-04 09:39:34 +0900

    [Gold V] Title: 배, Time: 224 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,51 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> crane = new ArrayList<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++)
-            crane.add(Integer.parseInt(st.nextToken()));
 
-        int M = Integer.parseInt(br.readLine());
-        List<Integer> box = new ArrayList<>();
-
+        int n = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<M; i++)
-            box.add(Integer.parseInt(st.nextToken()));
 
-        //내림차순으로 정렬
-        crane.sort(Collections.reverseOrder());
-        box.sort(Collections.reverseOrder());
+        List<Integer> crains = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            crains.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
 
-        if(crane.get(0)<box.get(0)){
+        List<Integer> boxes = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            boxes.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(crains, Collections.reverseOrder());
+        Collections.sort(boxes, Collections.reverseOrder());
+        if (crains.get(0) < boxes.get(0)) {
             System.out.println(-1);
             return;
         }
-
-        int day = 0;
-        while(!box.isEmpty()){
-            int boxIdx = 0, craneIdx = 0;
-
-            while(craneIdx<N){
-                if(boxIdx == box.size())
-                    break;
-                else if(crane.get(craneIdx) >= box.get(boxIdx)){
-                    box.remove(boxIdx);
-                    craneIdx++;
-                }
-                else
+        int time = 0;
+        while (!boxes.isEmpty()) {
+            int craIdx = 0;
+            int boxIdx = 0;
+            while (craIdx < n) {
+                if (boxIdx == boxes.size()) break;
+                if (crains.get(craIdx) >= boxes.get(boxIdx)) {
+                    boxes.remove(boxIdx);
+                    craIdx++;
+                } else {
                     boxIdx++;
+                }
             }
-
-            day++;
+            time++;
         }
 
-        System.out.println(day);
+        System.out.println(time);
     }
 }
