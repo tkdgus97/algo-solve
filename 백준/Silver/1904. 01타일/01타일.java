c62b3c08687c84bdd262ce35428a024d67875859
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-11-25 12:52:56 +0900

    [Silver III] Title: 01타일, Time: 80 ms, Memory: 15444 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = stoi(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+        System.out.println(dp[n]);
+    }
+
+    private static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+}
