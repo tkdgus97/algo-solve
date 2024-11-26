1e4e6e839b697633033197e7f295e044c407f016
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-11-26 09:57:52 +0900

    [Silver III] Title: 등수 매기기, Time: 516 ms, Memory: 61004 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -11,26 +11,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         int n = stoi(br.readLine());
-
-        int[] rank = new int[n + 1];
-
         long rSum = 0;
-        int lIdx = 1;
         int[] arr = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             int r = stoi(st.nextToken());
             arr[i] = r;
         }
-
         Arrays.sort(arr);
-
         for (int i = 1; i <= n; i++) {
-            if (arr[i] != i) {
-                rSum += Math.abs(arr[i]- i);
-            }
+             rSum += Math.abs(arr[i]- i);
         }
 
         System.out.println(rSum);
