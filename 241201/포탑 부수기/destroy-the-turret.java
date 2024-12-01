8d902e03acb3921bbbdba3f7cffa19e9c2f64743
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2024-12-01 23:22:24 +0900

    Title: 포탑 부수기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241201/포탑 부수기/destroy-the-turret.java
+++ 241201/포탑 부수기/destroy-the-turret.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.io.*;
 public class Main {
     private static int n,m,k;
-    private static int[][] map;
-    private static int[][] lastAttack;
+    private static Tower[][] map;
     private static int[] dx = {0,1,0,-1, -1,-1,1,1};
     private static int[] dy = {1,0,-1,0, -1,1,1,-1};
 
-    private static List<Tower> list = new ArrayList<>();
+    private static List<Tower> list;
     private static boolean[][] isDamage;
 
     static class Tower implements Comparable<Tower> {
@@ -27,7 +26,7 @@ public class Main {
         public int compareTo(Tower t) {
             if(this.atk != t.atk) return this.atk - t.atk;
             if(this.time != t.time) return t.time - this.time;
-            if((this.r + this.c) != (t.r + t.c)) return (t.r + t.c) - (this.r + this.c);
+            if(this.r + this.c != t.r + t.c) return (t.r + t.c) - (this.r + this.c);
             return t.c - this.c;
         }
     }
@@ -50,87 +49,83 @@ public class Main {
         m = stoi(st.nextToken());
         k = stoi(st.nextToken());
 
-        map = new int[n][m];
-        lastAttack = new int[n][m];
+        map = new Tower[n][m];
 
         for(int r = 0; r < n; r++) {
             st = new StringTokenizer(br.readLine());
             for(int c = 0; c < m; c++) {
-                map[r][c] = stoi(st.nextToken());
+                int v = stoi(st.nextToken());
+                map[r][c] = new Tower(r,c,v,0);
             }
         }
 
         simul();
-        init();
-        Collections.sort(list);
-        System.out.println(list.get(list.size() - 1).atk);
+
+        int max = Integer.MIN_VALUE;
+        for(int r = 0; r < n; r++) {
+            for(int c = 0; c < m; c++) {
+                max = Math.max(map[r][c].atk, max);
+            }
+        }
+
+        System.out.println(max);
     }
 
     private static void simul() {
-        while(k-- > 0) {
+        for(int i = 0; i < k; i++){
             init();
-
-            Collections.sort(list);
 
             if(list.size() <= 1) break;
 
-            Tower atack = findAtackTower();
-            Tower target = findTargetTower();
-            isDamage[atack.r][atack.c] = true;
-            isDamage[target.r][target.c] = true;
-            // System.out.println("=========================");
-            // System.out.println("attack : " + atack.r + " " + atack.c);
-            // System.out.println("tartget : " +target.r + " " + target.c);
+            Tower attack = findAttack();
+            Tower target = findTarget();
 
-            if(!laser(atack, target)) {
-                boom(atack, target);
+            if(!laser(attack, target)) {
+                boom(attack, target);
             }
 
-            removeTower();
+            destroyTower();
             repair();
-            // for(int r = 0; r < n; r++) {
-            //     System.out.println(Arrays.toString(map[r]));
-            // }
         }
     }
 
-    private static Tower findAtackTower() {
-        Tower atack = list.get(0);
-        atack.atk = atack.atk + (n + m);
-        map[atack.r][atack.c] += (n + m);
-        lastAttack[atack.r][atack.c]++; 
-        return atack;
+    private static Tower findAttack() {
+        Tower t = list.get(0);
+        t.time++;
+        t.atk += (n + m);
+        isDamage[t.r][t.c] = true;
+        return list.get(0);
     }
 
-    private static Tower findTargetTower() {
-        return list.get(list.size() - 1);
+    private static Tower findTarget() {
+        Tower t = list.get(list.size() - 1);
+        isDamage[t.r][t.c] = true;
+        return t;
     }
 
-    private static boolean laser(Tower atack, Tower target) {
+
+
+    private static boolean laser(Tower attack, Tower target) {
         boolean[][] visit = new boolean[n][m];
-
-        Queue<Point> q = new LinkedList<>();
         Point[][] route = new Point[n][m];
-
-        visit[atack.r][atack.c] = true;
-        q.add(new Point(atack.r, atack.c));
-
+        Queue<Point> q = new LinkedList<>();
+        visit[attack.r][attack.c] = true;
+        q.add(new Point(attack.r, attack.c));
         boolean flag = false;
-        route[atack.r][atack.c] = new Point(atack.r, atack.c);
-
         while(!q.isEmpty()) {
-            Point now = q.poll();
-            if(target.r == now.r && target.c == now.c) {
+            Point cur = q.poll();
+
+            if(cur.r == target.r && cur.c == target.c) {
                 flag = true;
                 break;
             }
 
             for(int i = 0; i < 4; i++) {
-                int nx = (now.r + dx[i] + n) % n;
-                int ny = (now.c + dy[i] + m) % m;
+                int nx = (cur.r + dx[i] + n) % n;
+                int ny = (cur.c + dy[i] + m) % m;
 
-                if(!visit[nx][ny] && map[nx][ny] != 0) {
-                    route[nx][ny] = new Point(now.r, now.c);
+                if(!visit[nx][ny] && map[nx][ny].atk != 0) {
+                    route[nx][ny] = new Point(cur.r, cur.c);
                     visit[nx][ny] = true;
                     q.add(new Point(nx, ny));
                 }
@@ -138,38 +133,36 @@ public class Main {
         }
 
         if(flag) {
-            map[target.r][target.c] -= atack.atk;
+            map[target.r][target.c].atk -= attack.atk;
             Point cur = route[target.r][target.c];
-            while(true) {
-                // System.out.println(cur.r + " " + cur.c);
+            while(cur.r != attack.r || cur.c != attack.c) {
                 isDamage[cur.r][cur.c] = true;
-                if(cur.r == atack.r && cur.c == atack.c) break;
-                map[cur.r][cur.c] -= (atack.atk / 2);
+                map[cur.r][cur.c].atk -= (attack.atk / 2);
                 cur = route[cur.r][cur.c];
             }
-            return true;
         }
-        return false;
+
+        return flag;
     }
 
-    private static void boom(Tower atack, Tower target) {
-        map[target.r][target.c] -= atack.atk;
+    private static void boom(Tower attack, Tower target) {
+        map[target.r][target.c].atk -= attack.atk;
         for(int i = 0; i < 8; i++) {
             int nx = (target.r + dx[i] + n) % n;
             int ny = (target.c + dy[i] + m) % m;
 
-            if(map[nx][ny] > 0) {
+            if(map[nx][ny].atk != 0 && nx != attack.r && ny != attack.c) {
                 isDamage[nx][ny] = true;
-                map[nx][ny] -= (atack.atk / 2);
+                map[nx][ny].atk -= (attack.atk / 2);
             }
         }
     }
 
-    private static void removeTower() {
+    private static void destroyTower() {
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
-                if(map[r][c] < 0) {
-                    map[r][c] = 0;
+                if(map[r][c].atk < 0) {
+                    map[r][c].atk = 0;
                 }
             }
         }
@@ -178,24 +171,28 @@ public class Main {
     private static void repair() {
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
-                if(map[r][c] > 0 && !isDamage[r][c]) {
-                    map[r][c] += 1;
+                if(map[r][c].atk > 0 && !isDamage[r][c]) {
+                    map[r][c].atk++;
                 }
             }
         }
     }
 
     private static void init() {
-        isDamage = new boolean[n][m];
         list = new ArrayList<>();
+        isDamage = new boolean[n][m];
+
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
-                if(map[r][c] > 0) {
-                    list.add(new Tower(r, c, map[r][c], lastAttack[r][c]));
+                if(map[r][c].atk > 0) {
+                    list.add(map[r][c]);
                 }
             }
         }
+
+        Collections.sort(list);
     }
+
 
     private static int stoi(String s) {
         return Integer.parseInt(s);
