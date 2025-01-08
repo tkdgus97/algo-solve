d3813fe9772cb5d5a1775a04425ec52a4b3b27d4
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2025-01-08 10:36:04 +0900

    [Silver II] Title: 나무 자르기, Time: 456 ms, Memory: 168648 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        int n = stoi(st.nextToken());
+        int m = stoi(st.nextToken());
+        int[] arr = new int[n];
+
+        int r = Integer.MIN_VALUE;
+        int l = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = stoi(st.nextToken());
+            r = Math.max(r, arr[i]);
+        }
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] - mid > 0) {
+                    sum += (arr[i] - mid);
+                }
+            }
+
+            if (sum >= m) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        System.out.println(r);
+    }
+
+    private static int stoi(String v) {
+        return Integer.parseInt(v);
+    }
+
+}
