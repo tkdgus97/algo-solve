da2545aafdcb34afbb60809a258b0b343ade8f80
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2025-01-13 09:43:06 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 636 ms, Memory: 160372 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,79 +1,74 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.*;
 
-//문제 풀이용
 public class Main {
+
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static int n, m;
+    private static int[][] map;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        n = stoi(st.nextToken());
+        m = stoi(st.nextToken());
 
-        int[][] map = new int[N][M];
+        map = new int[n][m];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String s = st.nextToken();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Character.getNumericValue(s.charAt(j));
-
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j) - '0';
             }
         }
 
-        int result = BFS(map, N, M);
-
-        System.out.println(result);
+        int result = bfs();
+        System.out.println(result == -1 ? result : result + 1);
     }
 
-    private static int BFS(int[][] map, int N, int M) {
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
+    private static int bfs() {
+        boolean[][][] visit = new boolean[n][m][2];
+        Queue<int[]> q = new LinkedList<>();
+        visit[0][0][1] = true;
 
-        boolean[][][] visit = new boolean[N][M][2];
-        Queue<int[]> Q = new ArrayDeque<>();
-
-        visit[0][0][0] = true;
-        Q.add(new int[]{0, 0, 1, 1});
-        int result = -1;
-        while (!Q.isEmpty()) {
-            int[] now = Q.poll();
-
-            if (now[0] == N - 1 && now[1] == M - 1) {
-                return now[2];
-            }
-
+        q.add(new int[]{0, 0, 0, 1});
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            if (now[0] == (n - 1) && now[1] == (m - 1)) return now[2];
             for (int i = 0; i < 4; i++) {
                 int nx = now[0] + dx[i];
                 int ny = now[1] + dy[i];
 
-                if ((nx >= 0 && nx < N) && (ny >= 0 && ny < M)) {
-                    //벽 부수기 가능
-                    if (now[3] == 1) {
-                        if (map[nx][ny] == 0 && !visit[nx][ny][0]) {
-                            visit[nx][ny][0] = true;
-                            Q.add(new int[]{ nx, ny, now[2] + 1, now[3]});
-                        } else if (map[nx][ny] == 1 && !visit[nx][ny][1]){
-                            visit[nx][ny][1] = true;
-                            Q.add(new int[]{ nx, ny, now[2] + 1, 0});
+                if (check(nx, ny) && !visit[nx][ny][now[3]]) {
+                    if (map[nx][ny] == 1) {
+                        if (now[3] > 0) {
+                            visit[nx][ny][now[3] - 1] = true;
+                            q.add(new int[]{nx, ny, now[2] + 1, now[3] - 1});
                         }
                     } else {
-                        if (map[nx][ny] == 0 && !visit[nx][ny][1]) {
-                            visit[nx][ny][1] = true;
-                            Q.add(new int[]{ nx, ny, now[2] + 1, now[3]});
-                        }
+                        visit[nx][ny][now[3]] = true;
+                        q.add(new int[]{nx, ny, now[2] + 1, now[3]});
                     }
                 }
             }
-
         }
 
-        return result;
+        return -1;
+    }
+
+    private static boolean check(int nx, int ny) {
+        return nx >= 0 && nx < n && ny >= 0 && ny < m;
+    }
+
+    private static int stoi(String v) {
+        return Integer.parseInt(v);
     }
 
 }
