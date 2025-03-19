b5f05ecc9e72542b9f4560fd6354b010d7d60a8f
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2025-03-19 11:07:17 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1716 ms, Memory: 85988 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[] p;
+    private static List<Integer>[] tree;
+    private static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int n = stoi(st.nextToken());
+
+        tree = new List[n + 1];
+        p = new int[n + 1];
+        visit = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = stoi(st.nextToken());
+            int e = stoi(st.nextToken());
+            tree[s].add(e);
+            tree[e].add(s);
+        }
+
+        p[1] = 1;
+        dfs(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(p[i]);
+        }
+    }
+
+    private static void dfs(int v) {
+        visit[v] = true;
+
+        for (Integer node : tree[v]) {
+            if (!visit[node]) {
+                p[node] = v;
+                dfs(node);
+            }
+        }
+    }
+
+
+    private static int stoi(String v) {
+        return Integer.parseInt(v);
+    }
+}
