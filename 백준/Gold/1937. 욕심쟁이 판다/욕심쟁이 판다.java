a3e32c7085f3d090c1dc29ed8eadf48d6feec5ec
tkdgus97 <106962275+tkdgus97@users.noreply.github.com>
2025-03-29 13:29:54 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 380 ms, Memory: 40444 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,61 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.*;
 
-//문제 풀이용
 public class Main {
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+    private static int n;
+    private static int[][] map;
 
-    private static int[][] map, dp;
-    private static int N;
-
+    private static int[][] dp;
+    private static int[] dx = {-1,0,1,0};
+    private static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-//        int M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][N];
-        dp = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
+        n = stoi(st.nextToken());
+        map = new int[n][n];
+        dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                max = Math.max(dfs(i,j), max);
+            for (int j = 0; j < n; j++) {
+                map[i][j] = stoi(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
-        System.out.println(max);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dp[i][j] == -1) {
+                    dfs(i,j);
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+
+        System.out.println(result);
     }
 
-    private static int dfs(int x, int y) {
-        if (dp[x][y] != 0) {
-            return dp[x][y];
+    private static int dfs(int r, int c) {
+        if (dp[r][c] != -1) {
+            return dp[r][c];
         }
 
-        dp[x][y] = 1;
-
+        dp[r][c] = 1;
+        int v = 1;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = r + dx[i];
+            int ny = c + dy[i];
 
-            if ((nx >= 0 && nx < N) && (ny >= 0 && ny < N) && map[x][y] < map[nx][ny]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            if (check(nx, ny) && map[r][c] < map[nx][ny]) {
+                v = Math.max(v, dfs(nx, ny) + dp[r][c]);
             }
         }
+        dp[r][c] = v;
+        return dp[r][c];
+    }
 
-        return dp[x][y];
+    private static boolean check(int nx, int ny) {
+        return nx >= 0 & nx < n && ny >= 0 && ny < n;
+    }
+
+    private static int stoi(String v) {
+        return Integer.parseInt(v);
     }
 }
